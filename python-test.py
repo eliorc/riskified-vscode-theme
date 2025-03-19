5e0b543b4aebf32458d43d2da4ead1086ede39a4
Elior Cohen <elior.cohen@riskified.com>
2025-03-19 16:57:40 +0200

    Initial commit

---
+++ python-test.py
@@ -0,0 +1,30 @@
+def hello():
+    print("Hello, World!")
+
+
+hello()
+
+print("yes")
+
+
+class Person:
+    def __init__(self, name: str, age):
+        # What do you mean
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"Person(name={self.name}, age={self.age})"
+
+    def say_bye(self):
+        print(f"Bye, my name is {self.name} and I am {self.age} years old.")
+
+    def say_hello(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+
+person = Person("John", 30)
+person.say_hello()
+
+
+print(2)
