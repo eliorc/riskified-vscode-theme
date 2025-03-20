981b042ceec3d0bdf483b506cc048fbfd7b89a6f
Elior Cohen <elior.cohen@riskified.com>
2025-03-20 09:15:39 +0200

    working version

--- python-test.py
+++ python-test.py
@@ -6,12 +6,24 @@ hello()
 
 print("yes")
 
+"""
+LONG STRING
+"""
+
 
 class Person:
     def __init__(self, name: str, age):
         # What do you mean
         self.name = name
         self.age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
 
     def __str__(self):
         return f"Person(name={self.name}, age={self.age})"
